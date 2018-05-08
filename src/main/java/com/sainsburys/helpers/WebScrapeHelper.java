03dabcf7a5e63cf293a18bbe1d0dc6d3c470d7d3
Peter Thomson <thomsonp@thehutgroup.com>
2018-05-08 14:40:54 +0100

    Refactor of WebScrapeHelper class so that testing can take place for the Sainsbury's test scrape exercise.

--- src/main/java/com/sainsburys/helpers/WebScrapeHelper.java
+++ src/main/java/com/sainsburys/helpers/WebScrapeHelper.java
@@ -20,7 +20,12 @@ public class WebScrapeHelper {
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
             if (inputLine.contains("productInfo")) {
-                analyseForUrlInformation(allProducts, in);
+                String input = "";
+                do {
+                    input = in.readLine();
+                    analyseForUrlInformation(allProducts, input);
+                    input = input.trim();
+                } while (!input.equals("</div>"));
             }
         }
         in.close();
@@ -28,23 +33,19 @@ public class WebScrapeHelper {
         return allProducts;
     }
 
-    private void analyseForUrlInformation(ArrayList<Product> allProducts, BufferedReader in) throws IOException {
-        String input = "";
-        do {
-            input = in.readLine();
-            if (input.contains("<a href=")) {
-                input = input.replace("<a href=\"", Statics.EMPTY_STRING)
-                        .replace("\" >", Statics.EMPTY_STRING)
-                        .replace(Statics.RELATIVE_BASE_URL, Statics.EMPTY_STRING)
-                        .trim();
+    public void analyseForUrlInformation(ArrayList<Product> allProducts, String input) throws IOException {
 
-                allProducts.add(getProductDetails(Statics.BASE_PRODUCT_URL + input));
-            }
-            input = input.trim();
-        } while (!input.equals("</div>"));
+        if (input.contains("<a href=")) {
+            input = input.replace("<a href=\"", Statics.EMPTY_STRING)
+                    .replace("\" >", Statics.EMPTY_STRING)
+                    .replace(Statics.RELATIVE_BASE_URL, Statics.EMPTY_STRING)
+                    .trim();
+
+            allProducts.add(getProductDetails(Statics.BASE_PRODUCT_URL + input));
+        }
     }
 
-    private Product getProductDetails(String revisedUrl) throws IOException {
+    public Product getProductDetails(String revisedUrl) throws IOException {
 
         Product product = new Product();
 
@@ -52,50 +53,51 @@ public class WebScrapeHelper {
 
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            analyseForProductData(product, in, inputLine);
+            if (inputLine.contains("productSummary")) {
+                String input = "";
+                do {
+                    input = in.readLine();
+                    analyseForProductData(product, input, in);
+                } while (!input.contains("</html>"));
+            }
         }
         in.close();
 
         return product;
     }
 
-    private void analyseForProductData(Product product, BufferedReader in, String inputLine) throws IOException {
-        if (inputLine.contains("productSummary")) {
-            String input = "";
+    public void analyseForProductData(Product product, String input, BufferedReader in) throws IOException {
+
+        if (input.contains("<h1>")) {
+            input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING).trim();
+            product.setTitle(input);
+        }
+        if (input.contains("£") && input.contains("pricePerUnit")) {
+            input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING)
+                    .replace("/unit", Statics.EMPTY_STRING)
+                    .replace("£", Statics.EMPTY_STRING)
+                    .trim();
+            product.setUnitPrice(Double.parseDouble(input));
+        }
+        if (input.contains("<h3") && input.contains("Description")) {
             do {
                 input = in.readLine();
-                if (input.contains("<h1>")) {
+                if (input.contains("<p>") && input.contains("</p>")) {
                     input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING).trim();
-                    product.setTitle(input);
+                    product.setDescription(input);
+                    break;
                 }
-                if (input.contains("£") && input.contains("pricePerUnit")) {
-                    input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING)
-                            .replace("/unit", Statics.EMPTY_STRING)
-                            .replace("£", Statics.EMPTY_STRING)
-                            .trim();
-                    product.setUnitPrice(Double.parseDouble(input));
-                }
-                if (input.contains("<h3") && input.contains("Description")) {
-                    do {
-                        input = in.readLine();
-                        if (input.contains("<p>") && input.contains("</p>")) {
-                            input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING).trim();
-                            product.setDescription(input);
-                            break;
-                        }
-                    } while (true);
-                }
-                if (input.contains("kJ") && (input.contains("Energy") || input.contains("Energy kJ"))) {
-                    input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING)
-                            .replace("Energy", Statics.EMPTY_STRING)
-                            .replace("kJ", Statics.EMPTY_STRING)
-                            .replace("-", Statics.EMPTY_STRING)
-                            .trim();
-                    StringBuffer sb = new StringBuffer();
-                    sb.append(input.charAt(0)).append(input.charAt(1)).append(input.charAt(2));
-                    product.setCalories(sb.toString() + "kJ");
-                }
-            } while (!input.equals("</html>"));
+            } while (true);
+        }
+        if (input.contains("kJ") && (input.contains("Energy") || input.contains("Energy kJ"))) {
+            input = input.replaceAll(Statics.HTML_TAG_REGEX, Statics.EMPTY_STRING)
+                    .replace("Energy", Statics.EMPTY_STRING)
+                    .replace("kJ", Statics.EMPTY_STRING)
+                    .replace("-", Statics.EMPTY_STRING)
+                    .trim();
+            StringBuffer sb = new StringBuffer();
+            sb.append(input.charAt(0)).append(input.charAt(1)).append(input.charAt(2));
+            product.setCalories(sb.toString() + "kJ");
         }
     }
 
